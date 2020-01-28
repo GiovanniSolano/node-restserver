82d6ac1dfa43ce0db69b2745662e5a9fe8ef712a
Giovanni Solano <giovannisolano211@gmail.com>
2020-01-28 16:09:14 -0600

    Variable de entorno mongo

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb+srv://gio:tzW7gbn3b4ORpRGD@cluster0-waf24.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 
 }
 
